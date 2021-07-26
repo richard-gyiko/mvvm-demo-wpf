77241644adfdbdb0a583520b2489c7d19a1e6bab
Richard Gyiko <richard@seetickets.us>
2021-07-26 16:05:26 +0200

    Add Api client generator and generated client

---
+++ PetStore/tools/ApiClientGenerator/Program.cs
@@ -0,0 +1,52 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using NSwag.CodeGeneration.OperationNameGenerators;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiClientGenerator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var petStoreApiClient = await GenerateClientCodeFromOpenApiSpecs(
+                specsUrl: "https://petstore.swagger.io/v2/swagger.json",
+                clientName: "PetStoreApiClient",
+                @namespace: "PetStore.ApiClient",
+                generateExceptionClasses: true);
+
+            ;
+        }
+
+        private static async Task<string> GenerateClientCodeFromOpenApiSpecs(string specsUrl, string clientName, string @namespace, bool generateExceptionClasses = false)
+        {
+            OpenApiDocument doc;
+
+            using (var webClient = new WebClient())
+            {
+                doc = await OpenApiDocument.FromJsonAsync(webClient.DownloadString(specsUrl));
+            }
+
+            var settings = new CSharpClientGeneratorSettings
+            {
+                ClassName = clientName,
+                InjectHttpClient = true,
+                UseBaseUrl = false,
+                GenerateClientInterfaces = true,
+                GenerateExceptionClasses = generateExceptionClasses,
+                CSharpGeneratorSettings =
+                {
+                    Namespace = @namespace,
+                    DateTimeType = "System.DateTime",
+                    DateType = "System.DateTime",
+                    GenerateNullableReferenceTypes = false,
+                }
+            };
+
+            var generator = new CSharpClientGenerator(doc, settings);
+
+            return generator.GenerateFile();
+        }
+    }
+}
